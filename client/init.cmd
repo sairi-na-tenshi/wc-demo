4837b9e150abdad8d41bb21c14a37b9d1b4cf23a
sairi-na-tenshi <sairi.na.tenshi@gmail.com>
1999-10-12 13:49:29 +0400

    some fixes in client

--- client/init.cmd
+++ client/init.cmd
@@ -1,3 +1,1 @@
-git init
-git pull git://github.com/sairi-na-tenshi/web-component.git
-git submodule init
+git init && git pull git://github.com/sairi-na-tenshi/web-component.git && git submodule init && git submodule update
